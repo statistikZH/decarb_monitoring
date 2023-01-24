a0b9480b24af39383aad6e0811852dfd602838ab
tknecht <thomas.knecht@statistik.ji.ktzh.ch>
2023-01-24 14:55:00 +0000

    extract also the max year

--- R/download_helpers.R
+++ R/download_helpers.R
@@ -4,17 +4,24 @@
 #' !important: this bfs-nr is not the municipality bfs-nr
 #'
 #' @param bfs_nr Number of a bfs publication e.g: "ind-d-21.02.30.1202.02.01"
-get_bfs_asset_nr <- function(bfs_nr) {
+get_bfs_asset_nr <- function(ds) {
   bfs_home <- "https://www.bfs.admin.ch"
 
-  asset_page <- xml2::read_html(paste0(bfs_home, "/asset/de/", bfs_nr))
+  asset_page <- xml2::read_html(paste0(bfs_home, "/asset/de/", ds$data_id))
 
-  asset_number <- asset_page %>%
-    rvest::html_text(bfs_nr) %>%
+  ds$asset_number <- asset_page %>%
+    rvest::html_text(ds$data_id) %>%
     stringr::str_extract("https://.*assets/.*/") %>%
     stringr::str_extract("[0-9]+")
 
-  return(asset_number)
+  ds$year_end <- asset_page %>%
+    rvest::html_element("table") %>%
+    rvest::html_table() %>%
+    dplyr::filter(X1 == "Dargestellter Zeitraum") %>%
+    dplyr::pull(X2) %>%
+    substr(., nchar(.) - 3, nchar(.))
+
+  return(ds)
 }
 
 #' Function that generates the query list for PX data formats.
@@ -33,15 +40,18 @@ get_px_query_list <- function(ds) {
 
   query_list <- list()
 
-  # ## AAAAACHTUNG!!
-  # if(ds$data_id == "G1"){
-  #   ds$year_start <- 2021
-  # }
+  # FIXME: At the next update check labels for the year!!
+  # if it is still coded, we need to get the code for the max year
+  # if the codes were changed to the actual year, we can get rid of the
+  # if else clause
+  if(ds$dataset_id == "G1"){
+    query_list[[ds$year_col]] <- as.character(ds$year_start)
+  }else{
+    query_list[[ds$year_col]] <- as.character(
+      ds$year_start:ds$year_end
+    )
+  }
 
-  # Year does not work for G1 yet
-  query_list[[ds$year_col]] <- as.character(
-    ds$year_start:lubridate::year(Sys.Date()- lubridate::years(2))
-  )
   query_list[[ds$gebiet_col]] <- stringr::str_split(ds$gebiet_id, ",")[[1]]
   query_list[[ds$dimension_col]] <- stringr::str_split(ds$dimension_id, ",")[[1]]
 
@@ -75,14 +85,26 @@ get_download_url.default <- function(ds) {
 #' @export
 get_download_url.bfs <- function(ds) {
 
-  # get asset number
-  asset_number <- get_bfs_asset_nr(ds$data_id)
-
-  # set download path
-  ds$download_url <- paste0(ds$data_url, asset_number, "/master")
+  ds <- get_download_url_bfs(ds)
 
   return(ds)
 }
 
+get_download_url_bfs <- function(ds) UseMethod("get_download_url_bfs")
 
 
+get_download_url_bfs.px <- function(ds){
+  ds$download_url <- paste0(ds$data_url, ds$data_id, "/", ds$data_id, ".px")
+
+  return(ds)
+}
+
+get_download_url_bfs.default <- function(ds){
+  # get asset number
+  ds <- get_bfs_asset_nr(ds)
+
+  # set download path
+  ds$download_url <- paste0(ds$data_url, ds$asset_number, "/master")
+
+  return(ds)
+}
