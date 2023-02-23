db84b227d5a4ad786594c66da6e9e3119710cd56
philbosch <philippmartinbosch@gmail.com>
2023-02-23 16:00:36 +0000

    M2 computations (begin) + new parameter file (fixed M2 px path)

---
+++ scripts/M2_computations.R
@@ -0,0 +1,61 @@
+# M5 - Fossilfreie Neuzulassungen Güterverkehr ----------------------------------------------------
+
+## Computations:
+## 1. Anzahl pro Gebiet
+## 2. Anteil pro Gebiet und Jahr
+
+# Import data -------------------------------------------------------------
+
+ds <- create_dataset("M2")
+ds <- download_data(ds)
+
+m5_data <- ds$data
+
+# Computation: Anzahl & Anteil -----------------------------------------------------
+
+## Splitting Fernwärme into fossil and fossil-free
+## Assigning 10% of heating to means of fossil fuel
+m5_computed <- m5_data %>%
+  # Renaming of columns in preparation to bring data into a uniform structure
+  dplyr::rename("Gebiet" = Kanton, "Variable" = Treibstoff, "Wert" = `Neue Inverkehrsetzungen von Strassenfahrzeugen`) %>%
+  # Auxiliary variable for calculating the number of fossil vs. fossil-free passenger cars. Fossil being 'Benzin' + 'Diesel' + 'Gas (mono- und bivalent)'
+  dplyr::mutate(Treibstoff_Typ = dplyr::if_else(Variable %in% c("Benzin", "Diesel", "Gas (mono- und bivalent)"), "fossil", "fossil-free")) %>%
+  # Calculating number of cars by year, spacial unit, and fuel type
+  dplyr::group_by(Jahr, Gebiet, Treibstoff_Typ) %>%
+  dplyr::summarise(Anzahl = sum(Wert)) %>%
+  dplyr::ungroup() %>%
+  # Adding the total number of cars by year and spacial unit and calculate the share by fuel type
+  dplyr::group_by(Jahr, Gebiet) %>%
+  dplyr::mutate(Total = sum(Anzahl),
+                Anteil = (Anzahl / Total)) %>%
+  # Convert table to a long format
+  tidyr::pivot_longer(cols = c(Anzahl, Total, Anteil), names_to = "Einheit", values_to = "Wert") %>%
+  dplyr::ungroup()
+
+
+# Data structure ----------------------------------------------------------
+
+m5_export_data <- m5_computed %>%
+  dplyr::filter(Einheit != "Total") %>%
+  dplyr::rename("Variable" = Treibstoff_Typ) %>%
+  # Renaming values
+  dplyr::mutate(Gebiet = dplyr::if_else(Gebiet == "Zürich", "Kanton Zürich", Gebiet),
+                Variable = dplyr::if_else(Variable == "fossil", "fossiler Treibstoff", "fossilfreier Treibstoff"),
+                Einheit = dplyr::case_when(Einheit == "Anzahl" ~ paste0(ds$dimension_label, " [Anz.]"),
+                                           Einheit == "Anteil" ~ paste0(ds$dimension_label, " [%]"),
+                                           TRUE ~ Einheit)) %>%
+  # Manually adding columns for Indikator_ID, Indikator_Name, Einheit and Datenquelle
+  dplyr::mutate(Indikator_ID = ds$dataset_id,
+                Indikator_Name = ds$dataset_name,
+                Datenquelle = ds$data_source) %>%
+  dplyr::select(Jahr, Gebiet, Indikator_ID, Indikator_Name, Variable, Wert, Einheit, Datenquelle)
+
+# Export CSV --------------------------------------------------------------
+
+## Temporarily storing export files in Gitea repo > output folder
+## Naming convention for CSV files: [indicator number]_data.csv
+dir.create("output", showWarnings = FALSE)
+
+output_file <- paste0(ds$dataset_id, "_data.csv")
+
+utils::write.table(m5_export_data, paste0("./output/", output_file), fileEncoding = "UTF-8", row.names = FALSE, sep = ",")
