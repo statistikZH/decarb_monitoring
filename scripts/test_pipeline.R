2b926e24e40c9070b967516ee3d24c50b983827e
philbosch <philippmartinbosch@gmail.com>
2023-04-12 13:56:10 +0000

    new tests of pipeline

--- scripts/test_pipeline.R
+++ scripts/test_pipeline.R
@@ -4,7 +4,7 @@ ds_list <- purrr::map(ids, create_dataset)
 
 ds_new_list <- purrr::map(ds_list, download_data, .progress = TRUE)
 
-ds <- create_dataset("LF2")
+ds <- create_dataset("M1")
 ds <- download_data(ds)
 ds <- structure_data(ds)
 export_data(ds)
@@ -13,3 +13,10 @@ export_data(ds)
 
 
 
+readxl::read_excel("2773 Monitoring.xlsx", sheet = "ParameterlisteZH") %>%
+  dplyr::filter(STATUS == 1) %>%
+  dplyr::filter(stringr::str_detect(DATASET_ID, pattern = "^M\\d+(_\\d+)?$")) %>%
+  dplyr::pull(DATASET_ID) %>%
+  purrr::map(create_dataset) -> ds_list
+
+ds_download_list <- purrr::map(ds_list, download_data, .progress = TRUE)
