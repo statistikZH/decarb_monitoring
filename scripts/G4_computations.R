7e8b8dbcaf0cd165bc5e71e0d91e81ee7e981328
philbosch <philippmartinbosch@gmail.com>
2023-04-13 15:11:14 +0000

    script G4

--- scripts/G4_computations.R
+++ scripts/G4_computations.R
@@ -15,5 +15,24 @@ g4_data <- ds$data
 
 g4_export_data <- g4_data %>%
   dplyr::select(4:ncol(g4_data)) %>%
-  janitor::row_to_names(16) %>% View()
+  janitor::row_to_names(16) %>%
+  dplyr::slice(1) %>%
+  dplyr::mutate(dplyr::across(.fns = as.numeric)) %>%
+  tidyr::pivot_longer(cols = everything(), names_to = c("Jahr"), values_to = "Wert") %>%
+  dplyr::mutate(Gebiet = "Kanton Zürich",
+                Einheit = paste("Anzahl", ds$dataset_name,"[Anz.]", sep = " ")) %>%
+  # Manually adding columns for Indikator_ID, Indikator_Name, Einheit and Datenquelle
+  dplyr::mutate(Indikator_ID = ds$dataset_id,
+                Indikator_Name = ds$dataset_name,
+                Datenquelle = ds$data_source,
+                Variable = ds$dataset_name) %>%
+  dplyr::select(Jahr, Gebiet, Indikator_ID, Indikator_Name, Variable, Wert, Einheit, Datenquelle)
+
+ds$export_data <- g4_export_data
+
+# Export CSV --------------------------------------------------------------
+
+## Temporarily storing export files in Gitea repo > output folder
+## Naming convention for CSV files: [indicator number]_data.csv
+export_data(ds)
 
