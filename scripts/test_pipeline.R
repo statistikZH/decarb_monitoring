363acd6e5b83b4584c3934a85fcdf256805dc802
philbosch <philippmartinbosch@gmail.com>
2023-06-20 14:54:36 +0000

    adapt test script

--- scripts/test_pipeline.R
+++ scripts/test_pipeline.R
@@ -1,6 +1,10 @@
-ids <- c("A1", "G1", "Q1", "M1", "M8", "KG_a", "G3", "M2", "M3", "M4", "M5", "IG1", "LF2")
 
-ds_list <- purrr::map(ids, create_dataset)
+readxl::read_excel("2773 Monitoring.xlsx", sheet = "ParameterlisteZH") %>%
+  dplyr::filter(STATUS == 1) %>%
+  #dplyr::filter(stringr::str_detect(DATASET_ID, pattern = "^M\\d+(_\\d+)?$")) %>%
+  dplyr::pull(DATASET_ID) %>%
+  purrr::map(create_dataset) -> ds_list
+
 
 ds_new_list <- purrr::map(ds_list, download_data, .progress = TRUE)
 
@@ -13,10 +17,6 @@ export_data(ds)
 
 
 
-readxl::read_excel("2773 Monitoring.xlsx", sheet = "ParameterlisteZH") %>%
-  dplyr::filter(STATUS == 1) %>%
-  dplyr::filter(stringr::str_detect(DATASET_ID, pattern = "^M\\d+(_\\d+)?$")) %>%
-  dplyr::pull(DATASET_ID) %>%
-  purrr::map(create_dataset) -> ds_list
+
 
 ds_download_list <- purrr::map(ds_list, download_data, .progress = TRUE)
