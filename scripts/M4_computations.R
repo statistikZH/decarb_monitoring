81e11879a1ff9cc3432ffb0b20dba2313081b9cc
gma-lks <gian-marco.alt@bd.zh.ch>
2024-02-02 14:34:58 +0100

    M2/M3/M4: title from indicator_name, changed scripts and output

--- scripts/M4_computations.R
+++ scripts/M4_computations.R
@@ -70,7 +70,7 @@ m4_export_data <- m4_computed %>%
                                            TRUE ~ Einheit)) %>%
   # Manually adding columns for Indikator_ID, Indikator_Name, Einheit and Datenquelle
   dplyr::mutate(Indikator_ID = ds$dataset_id,
-                Indikator_Name = ds$dataset_name,
+                Indikator_Name = ds$indicator_name,
                 Datenquelle = ds$data_source) %>%
   dplyr::select(Jahr, Gebiet, Indikator_ID, Indikator_Name, Variable, Wert, Einheit, Datenquelle)
 
